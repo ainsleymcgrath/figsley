d71ced274aaa8fdc01052d53965ea4aaa4e267ad
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-01-28 12:29:53 -0600

    feat: list fonts

---
+++ src/routes/fonts/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Hi</h1>
+{#if data.fonts}
+	<ul>
+		{#each data.fonts as font}
+			<li>{font}</li>
+		{/each}
+	</ul>
+{/if}
