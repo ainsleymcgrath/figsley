77bce36861ddfdc11227148996fd7d82e31f3049
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-01-28 13:32:50 -0600

    drama

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,12 @@
 </script>
 
 <main class="h-screen w-screen bg-yellow-500 grid grid-cols-2 gap-3">
-	<header class="col-span-2">
-		<h1 class="text-2xl text-center">Figsley</h1>
+	<header class="col-span-2 h-10 text-center">
+		<h1 class="text-9xl font-display font-black">FIGSLEY</h1>
+		<p class="font-display font-light">Figlet for the modern web, by Ainsley</p>
 	</header>
-	<section class="col-span-1 h-full">
-		<article class="h-1/2">
+	<section class="col-span-1 grid">
+		<article class="w-4/5 justify-self-center">
 			<h2 class="text-center">Figlet Fonts</h2>
 			{#if data.fonts}
 				<ul class="overflow-scroll h-28 self-center bg-red-50">
