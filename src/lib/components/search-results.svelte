7e5b9ae0b252815684b015cd91166a901ce02b30
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-01-30 09:49:11 -0600

    more cleanup

--- src/lib/components/search-results.svelte
+++ src/lib/components/search-results.svelte
@@ -4,37 +4,13 @@
 	import type { FigletRecord } from '$lib/stores';
 
 	export let searchResults: FigletRecord[];
-	export let browsing: boolean = false;
-	// let list: HTMLOListElement;
-
-	// const focus = () => {
-	// 	list.focus();
-	// 	browsing = true;
-	// };
-	// const blur = () => {
-	// 	list.blur();
-	// 	browsing = false;
-	// };
-
-	let focusedSearchResultIndex: number = 0;
-	// const keydown = (e: KeyboardEvent) => {
-	// 	if (['Tab', 'ArrowUp', 'ArrowDown'].includes(e.key)) {
-	// 		browsing = true;
-	// 		e.preventDefault();
-	// 		if (e.key === 'ArrowUp' || (e.key === 'Tab' && e.shiftKey)) {
-	// 			focusedSearchResultIndex -= 1;
-	// 		} else {
-	// 			focusedSearchResultIndex += 1;
-	// 		}
-	// 	}
-	// };
 </script>
 
 <!-- on:keydown={keydown} -->
 <ol
 	class="overflow-scroll p-2 border-black border-4 rounded-lg bg-yellow-500 outline-0 focus:border-red-700"
 >
-	{#each searchResults as data, i}
-		<SearchResultOption focused={browsing && focusedSearchResultIndex === i} bind:data />
+	{#each searchResults as data}
+		<SearchResultOption bind:data />
 	{/each}
 </ol>
