cdb7a9f632b64be05a5bf7c947cf7adc28d5ea9e
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-01-30 21:30:58 -0600

    hover controls w icons

--- src/lib/ui/molecules/figlet-card.svelte
+++ src/lib/ui/molecules/figlet-card.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { fontRecordsByName } from '$lib/stores';
 	import type { Fonts } from 'figlet';
+	import Icon from '$lib/ui/atoms/icon.svelte';
 	export let title: Fonts;
 	export let rendered: string;
 	const copy = () => {
@@ -9,17 +10,30 @@
 	const deselct = () => {
 		$fontRecordsByName[title]!.selected = false;
 	};
+	let showButtons = false;
+	const mouseenter = () => {
+		showButtons = true;
+	};
+	const mouseleave = () => {
+		showButtons = false;
+	};
 </script>
 
-<figure>
+<figure class="h-min" on:mouseenter={mouseenter} on:mouseleave={mouseleave}>
 	<figcaption class="flex justify-between font-display p-1">
 		{title}
-		<button on:click={deselct} class="font-sans font-extralight text-xs opacity-50">X</button>
 	</figcaption>
 	<div class="border-4 border-black rounded-lg">
 		<pre class="font-mono leading-4 m-5">{rendered}</pre>
 	</div>
-	<div class="w-full flex justify-end">
-		<button on:click={copy} class="text-xs underline">Copy to clipboard</button>
+	<div class="w-full flex justify-end px-1">
+		{#if showButtons}
+			<button on:click={copy} class="icon-sm">
+				<Icon name="copy" />
+			</button>
+			<button on:click={deselct} class="icon-sm">
+				<Icon name="close" />
+			</button>
+		{/if}
 	</div>
 </figure>
