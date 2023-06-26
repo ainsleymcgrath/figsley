5c26a269ea9d72828cfceb0de5cf992222a0b9fe
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-06-26 18:50:16 -0500

    yay store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,4 @@
+import Fuse from 'fuse.js';
 import type { Fonts } from 'figlet';
 import { derived, writable } from 'svelte/store';
 
@@ -10,8 +11,67 @@ export interface FigletRecord {
   preview: string;
 }
 
-export const fontRecordsByName = writable<Partial<Record<Fonts, FigletRecord>>>({});
-export const fontRecords = derived(fontRecordsByName, (mapping) => Object.values(mapping));
-export const fontRecordsSelected = derived(fontRecords, (records) =>
-  records.filter((r) => r.selected)
-);
+function makeFontStore() {
+  const base = writable<Record<string, FigletRecord>>({});
+
+  const fuse = new Fuse<FigletRecord>([], { includeMatches: true, keys: ['font'] });
+  const metadata = derived(base, ($store) => {
+    const corpus = [...Object.values($store)];
+    fuse.setCollection(corpus);
+
+    return {
+      corpus,
+      fuse,
+      keys: Object.keys($store),
+      selections: Object.values($store).filter((v) => v.selected)
+    };
+  });
+
+  return {
+    // only read from the metadata
+    subscribe: metadata.subscribe,
+    // fuzzy search
+    search(searchTerm: string) {
+      const searchResultsRaw = fuse.search(searchTerm);
+      const searchResults = new Set(searchResultsRaw.map((r) => r.item.slug));
+
+      base.update(($store) => {
+        for (const slug of Object.keys($store)) {
+          $store[slug].hit = searchResults.has(slug);
+        }
+        return { ...$store };
+      });
+    },
+    // load data
+    seed(fonts: Fonts[]) {
+      base.update(() =>
+        fonts
+          .map(
+            (font) =>
+              ({
+                font,
+                selected: false,
+                searchMatchIndexes: [],
+                slug: font.toLowerCase().replace(' ', '-'),
+                hit: false,
+                preview: ''
+              } satisfies FigletRecord)
+          )
+          .reduce((acc, cur) => {
+            return { ...acc, [cur.slug]: { ...cur, hit: false } };
+          }, {})
+      );
+    },
+    // render a preview
+    updatePreview(fonts: FigletRecord[], preview: string) {
+      base.update(($store) => {
+        for (const font of fonts) {
+          $store[font.slug].preview = preview;
+        }
+        return { ...$store };
+      });
+    }
+  };
+}
+
+export const fontStore = makeFontStore();
